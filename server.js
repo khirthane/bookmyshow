eff005b311b286966f4c78d100f08db193173206
khirthane <khirthane@gmail.com>
2023-11-17 18:44:58 +0100

    index.html server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
+
 app.use(express.static(__dirname + "/dist/<my-app>"));
-app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname + "/dist/<my-app>/index.html"));
-});
+app.get("/*", (req, res) =>
+    res.sendFile("index.html", { root: "/dist/<my-app>/index.html" })
+);
 
 app.listen(process.env.PORT || 8080);
