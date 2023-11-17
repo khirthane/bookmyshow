78d64ca2fb3e3b7b68bc446924d8eecba15ad909
khirthane <khirthane@gmail.com>
2023-11-17 18:59:33 +0100

    updated server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(__dirname + "/dist/my-app"));
-app.get("/*", (req, res) =>
-    res.sendFile("index.html", { root: "/dist/my-app/index.html" })
-);
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname + "/dist/my-app/index.html"));
+});
 
 app.listen(process.env.PORT || 8080);
