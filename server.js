21cdbe0008a31803d8fb7baa6ac17ad63594d8bc
khirthane <khirthane@gmail.com>
2023-11-17 18:35:50 +0100

    Removed the server

--- server.js
+++ server.js
@@ -7,14 +7,3 @@ app.get("/*", function (req, res) {
 });
 
 app.listen(process.env.PORT || 8080);
-
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middleware = jsonServer.defaults();
-const port = 3000;
-
-server.use(middleware);
-server.use(router);
-
-server.listen(port);
